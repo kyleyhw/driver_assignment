67d049960cfd32ae27f5107ae07180568a1f42ab
Kyle <kyleyhw@gmail.com>
2025-02-11 23:44:10 +0000

    added basemap plotting but doesn't work

--- plot_map.py
+++ plot_map.py
@@ -1,40 +1,44 @@
-# import geopandas as gpd
-# import matplotlib.pyplot as plt
-#
-# # Load the shapefile (replace with your actual file path)
-# gdf = gpd.read_file('ex_sample.shp')
-#
-# # Plot the polygons
-# gdf.plot()
-# plt.show()
-
+import geopandas as gpd
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 from data_loader import get_coord_from_postcode, get_postcode_from_name, get_df
 from solve import solve
+import contextily as ctx
+from shapely.geometry import Point
 
 people_information_dataframe = get_df()
 driver_assignments = solve(people_information_dataframe=people_information_dataframe, verbose=True)
 
 colors = [*mcolors.TABLEAU_COLORS.values()]
 
-plt.figure()
+fig, ax = plt.subplots(1, 1)
+
+all_coords = []
 
 for i, driver_name in enumerate(driver_assignments.keys()):
     driver_postcode = get_postcode_from_name(name=driver_name, people_information_dataframe=people_information_dataframe)
     driver_coords = get_coord_from_postcode(driver_postcode)
-    plt.scatter(*driver_coords, c=colors[i], marker='x', label=driver_name)
+    ax.scatter(*driver_coords, c=colors[i], marker='x', label=driver_name)
+    all_coords.append(driver_coords)
 
     for passenger_name in driver_assignments[driver_name]:
         passenger_postcode = get_postcode_from_name(name=passenger_name, people_information_dataframe=people_information_dataframe)
         passenger_coords = get_coord_from_postcode(passenger_postcode)
-        plt.scatter(*passenger_coords, c=colors[i])
+        ax.scatter(*passenger_coords, c=colors[i])
+        all_coords.append(passenger_coords)
 
-plt.ylabel('Longitude')
-plt.xlabel('Latitude')
+# Create a GeoDataFrame
+gdf = gpd.GeoDataFrame(geometry=[Point(lon, lat) for lon, lat in all_coords], crs="EPSG:4326")
+# Convert to Web Mercator for contextily (EPSG:3857)
+gdf = gdf.to_crs(epsg=3857)
+# Add a basemap (choose a different provider if needed)
+ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron, crs=gdf.crs)
 
-plt.legend()
-plt.grid(which='both')
+ax.set_ylabel('Longitude')
+ax.set_xlabel('Latitude')
+
+ax.legend()
+ax.grid(which='both')
 
 plt.savefig('driver_assignment_plot.png')
 
