b82f530ff56fe92ba9b7018763f467ef347eaeaa
Kyle <kyleyhw@gmail.com>
2025-02-11 05:34:05 +0000

    defined header variables at top of file

--- data_loader.py
+++ data_loader.py
@@ -1,24 +1,32 @@
 import pandas as pd
 
+name_header = 'Name'
+postcode_header = 'Postcode'
+driver_header = 'Driver'
+driver_yes = '1'
+driver_no = '0'
+longitude_header = 'Longitude'
+latitude_header = 'Latitude'
+
 def get_people_df(filename='people.csv'):
     df = pd.read_csv(filename, delimiter=',', dtype='str')
     return df
 
 def get_driver_and_passenger_names(df):
-    driver_names = df[df['Driver'] == '1']['Name'].to_numpy(dtype='str')
-    passenger_names = df[df['Driver'] == '0']['Name'].to_numpy(dtype='str')
+    driver_names = df[df[driver_header] == driver_yes][name_header].to_numpy(dtype='str')
+    passenger_names = df[df[driver_header] == driver_no][name_header].to_numpy(dtype='str')
     return driver_names, passenger_names
 
 def get_postcode_from_name(name, df):
-    postcode = df[df['Name'] == name]['Postcode'].item()
+    postcode = df[df[name_header] == name][postcode_header].item()
     return postcode
 
 def get_coord_from_postcode(postcode):
     area_code_info = pd.read_csv('cb_area_codes.csv', delimiter=',')
 
     postcode_info_row = area_code_info.loc[area_code_info['Postcode'] == postcode]
-    longitude = float(postcode_info_row['Longitude'].values[0])
-    latitude = float(postcode_info_row['Latitude'].values[0])
+    longitude = float(postcode_info_row[longitude_header].values[0])
+    latitude = float(postcode_info_row[latitude_header].values[0])
 
     return longitude, latitude
 
